76eb0c3daade49fbdbc8ab0764e7f36342568a1b
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2022-06-26 19:23:31 +0200

    secondary button background change

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,21 @@ module.exports = {
     utils: true,
     prefix: "",
     darkTheme: "dark",
-    themes: ["dark"],
+    themes: [
+      {
+        dark: {
+          primary: "#6419E6",
+          secondary: "#be123c",
+          accent: "#1FB2A6",
+          neutral: "#191D24",
+          "base-100": "#2A303C",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 };
